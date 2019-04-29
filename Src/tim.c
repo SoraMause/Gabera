b0527cad2bef2b2015819ea771f8fa86be03d773
SoraMause <alcnemy71@gmail.com>
2019-04-29 14:33:58 +0900

    tim2のプリスケーラを31に変更、吸引力アップ

--- Src/tim.c
+++ Src/tim.c
@@ -97,7 +97,7 @@ void MX_TIM2_Init(void)
   TIM_OC_InitTypeDef sConfigOC = {0};
 
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 15;
+  htim2.Init.Prescaler = 31;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 99;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
